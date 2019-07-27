36600cea8af978b57c83d2a6b7b572775b785c21
kowalska.el <kowalska.el@gmail.com>
2019-07-27 18:28:10 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import * as Tone from 'tone';
+
+class Instrument {
+ constructor() {
+   this.synthType = null;
+   this.synth = null;
+   this.gain = new Tone.Gain();
+   this.gain.toMaster();
+ }
+
+ get defaultSettings() {
+   return {
+     Synth: {
+       oscillator: { type: 'sine' }
+     }
+   };
+ }
+
+ updateSynthType(synthType) {
+   let newSynth = new Tone[synthType]();
+   console.log(this.defaultSettings[synthType]);
+ }
+}
+
+let inst = new Instrument();
+inst.updateSynthType('Synth');
+
+
+
+

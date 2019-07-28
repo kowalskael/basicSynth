5097b81d94243d8e659a8fd7e6c321119e69045c
kowalska.el <kowalska.el@gmail.com>
2019-07-28 15:16:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as Tone from 'tone';
 
-const osc = new Tone.Oscillator(440, 'sine');
+const osc = new Tone.Oscillator(20, 'sine');
 const gain = new Tone.Gain();
 osc.sync().start();
 osc.connect(gain);
@@ -9,9 +9,16 @@ gain.toMaster();
 const $oscType = document.querySelector('#osc-type');
 $oscType.addEventListener('change', () => osc.type = $oscType.value);
 
+const $oscFreq = document.querySelector('#osc-freq');
+$oscFreq.addEventListener('input', () => osc.frequency.value = $oscFreq.value);
+
+const $oscPartialCounts = document.querySelector('#osc-partials');
+$oscPartialCounts.addEventListener('input', () => osc.partialCount = $oscPartialCounts.value);
+
 const $toggle = document.querySelector('#toggle');
 $toggle.addEventListener('click', function() {
 	Tone.Transport.Start = !Tone.Transport.Start;
 	if (Tone.Transport.Start) Tone.Transport.start();
 	else Tone.Transport.stop();
 });
+

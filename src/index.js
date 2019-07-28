038415c066df9637a8f36a0de7ed6c40037c2a16
kowalska.el <kowalska.el@gmail.com>
2019-07-28 14:42:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
 import * as Tone from 'tone';
 
-class Oscillator {
-  constructor() {
-    this.oscType = null;
-    this.osc = null;
-    this.gain = new Tone.Gain();
-    this.gain.toMaster();
-  }
+const osc = new Tone.Oscillator(440, 'sine');
+const gain = new Tone.Gain();
+osc.sync().start();
+osc.connect(gain);
+gain.toMaster();
 
-  get defaultSettings() {
-    return {
-      Oscillator:
-        {
-          type: 'sine',
-          frequency: 440 ,
-          detune: 0 ,
-          phase: 0 ,
-          partials: [] ,
-          partialCount: 0
-        }
-    };
-  }
+const $oscType = document.querySelector('#osc-type');
+$oscType.addEventListener('change', () => osc.type = $oscType.value);
 
-  updateOscType(oscType) {
-
-    if (this.osc) {
-      this.osc.disconnect(this.gain);
-      this.osc.dispose();
-    }
-
-    let settings = this.defaultSettings[oscType] || {};
-    this.osc = new Tone[oscType](settings);
-    this.osc.connect(this.gain);
-    this.osc.start();
-    console.log(settings);
-  }
-}
-
-let $oscType = document.querySelector('#osc-type');
-let osc = new Oscillator();
-osc.updateOscType($oscType.value);
-$oscType.addEventListener('change', e =>
-  osc.updateOscType($oscType.value)
-);
-
+const $toggle = document.querySelector('#toggle');
+$toggle.addEventListener('click', function() {
+	Tone.Transport.Start = !Tone.Transport.Start;
+	if (Tone.Transport.Start) Tone.Transport.start();
+	else Tone.Transport.stop();
+});

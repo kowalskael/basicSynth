201f310ae89d5808064c63da00f961db8252f205
kowalska.el <kowalska.el@gmail.com>
2019-07-28 11:15:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 import * as Tone from 'tone';
 
-class Instrument {
- constructor() {
-   this.synthType = null;
-   this.synth = null;
-   this.gain = new Tone.Gain();
-   this.gain.toMaster();
- }
+class Oscillator {
+  constructor() {
+    this.oscType = null;
+    this.osc = null;
+    this.gain = new Tone.Gain();
+    this.gain.toMaster();
+  }
 
- get defaultSettings() {
-   return {
-     Synth: {
-       oscillator: { type: 'sine' }
-     }
-   };
- }
+  get defaultSettings() {
+    return {
+      Oscillator:
+        {
+          type: 'sine',
+          frequency: 440 ,
+          detune: 0 ,
+          phase: 0 ,
+          partials: [] ,
+          partialCount: 0
+        }
+    };
+  }
 
- updateSynthType(synthType) {
-   let newSynth = new Tone[synthType]();
-   console.log(this.defaultSettings[synthType]);
- }
+  updateOscType(oscType) {
+
+    if (this.osc) {
+      this.osc.disconnect(this.gain);
+      this.osc.dispose();
+    }
+
+    let settings = this.defaultSettings[oscType] || {};
+    this.osc = new Tone[oscType](settings);
+    this.osc.connect(this.gain);
+    this.osc.start();
+    console.log(settings);
+  }
 }
 
-let inst = new Instrument();
-inst.updateSynthType('Synth');
-
-
-
-
+let $oscType = document.querySelector('#osc-type');
+let osc = new Oscillator();
+osc.updateOscType($oscType.value);
+$oscType.addEventListener('change', e =>
+  osc.updateOscType($oscType.value)
+);

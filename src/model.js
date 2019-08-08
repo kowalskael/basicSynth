fa75af106debc80026bfaa9a2f73f986f55df600
Ela Kowalska <kowalska.el@gmail.com>
2019-08-08 14:49:41 +0200

    no message

--- src/model.js
+++ src/model.js
@@ -1,30 +1,24 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import obj from './assets/elsynth_1.gltf';
+
+var targetRotation = 0;
+var targetRotationOnMouseDown = 0;
+var mouseX = 0;
+var mouseXOnMouseDown = 0;
+var windowHalfX = window.innerWidth / 2;
 
 const canvas = document.getElementById('c');
 const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 
-const camera = new THREE.PerspectiveCamera(50, 2, 0.1, 1000);
-camera.position.set(0, 0, 50);
+const camera = new THREE.PerspectiveCamera(10, 2, 0.1, 1000);
+camera.position.set(0, 0, 200);
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
-const controls = new OrbitControls( camera, renderer.domElement);
-
-controls.screenSpacePanning = false;
-controls.minDistance = 10;
-controls.maxDistance = 80;
-controls.maxAzimuthAngle = Math.PI / 3;
-controls.minAzimuthAngle = - Math.PI / 5;
-controls.maxPolarAngle = Math.PI / 1.5;
-controls.minPolarAngle = Math.PI / 3;
-controls.update();
-
 const spotLight = new THREE.SpotLight( 0xb5b5b5);
-spotLight.position.set( 0, 1000, 0 );
+spotLight.position.set( 0, 200, 150 );
 spotLight.penumbra = 2;
 scene.add(spotLight);
 
@@ -45,8 +39,6 @@ scene.add(loadModel);
 
 const loader = new GLTFLoader();
 
-let sustain;
-let decay;
 let attack;
 let release;
 
@@ -54,34 +46,55 @@ const mesh = new THREE.Object3D();
 loader.load(obj, gltf => {
   mesh.add(gltf.scene);
   loadModel.add(mesh);
-  console.log(dumpObject(mesh).join('\n'));
-  sustain = mesh.getObjectByName('ENV_Sustain');
-  decay = mesh.getObjectByName('ENV_Decay');
   attack = mesh.getObjectByName('ENV_Atack');
   release = mesh.getObjectByName('ENV_Release');
 });
 
-mesh.rotation.x = 0.7;
+mesh.rotation.x = 1.05;
 
-const render = (time) => {
-  time *= 0.001;
+document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 
-  if (sustain) {
-    sustain.rotation.y = time + 0.3;
+function onDocumentMouseDown( event ) {
+  event.preventDefault();
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+  document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+  mouseXOnMouseDown = event.clientX - windowHalfX;
+  targetRotationOnMouseDown = targetRotation;
+}
+function onDocumentMouseMove( event ) {
+  mouseX = event.clientX - windowHalfX;
+  targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+}
+function onDocumentMouseUp() {
+  document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+  document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+function onDocumentMouseOut() {
+  document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+  document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+}
+function onDocumentTouchStart( event ) {
+  if ( event.touches.length == 1 ) {
+    event.preventDefault();
+    mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+    targetRotationOnMouseDown = targetRotation;
   }
-
-  if (decay) {
-    decay.rotation.y = time - 3;
+}
+function onDocumentTouchMove( event ) {
+  if ( event.touches.length == 1 ) {
+    event.preventDefault();
+    mouseX = event.touches[ 0 ].pageX - windowHalfX;
+    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) ;
   }
+}
 
-  if (release) {
-    release.rotation.y = time;
-  }
-
-  if (attack) {
-    attack.rotation.y = time * 0.5;
-  }
-
+const render = () => {
+  attack.rotation.y += ( targetRotation - attack.rotation.y );
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 };
@@ -91,11 +104,14 @@ const resize = () => {
   renderer.setSize(clientWidth, clientHeight, false);
   camera.aspect = clientWidth / clientHeight;
   renderer.setPixelRatio(window.devicePixelRatio);
-  controls.update();
+
   camera.updateProjectionMatrix();
 };
 
 
-resize();
-render();
+THREE.DefaultLoadingManager.onLoad = () => {
+  window.addEventListener('resize', resize);
 
+  resize();
+  render();
+};
